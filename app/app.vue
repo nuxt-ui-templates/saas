ee8f3a79156ffcf7a0fb6d1bd4d136766f5f78d7
Benjamin Canac <canacb1@gmail.com>
2026-04-08 12:20:08 +0200

    fix(app): remove `twitterImage` meta

--- app/app.vue
+++ app/app.vue
@@ -20,7 +20,6 @@ useHead({
 useSeoMeta({
   titleTemplate: '%s - Nuxt SaaS template',
   ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/saas-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/saas-light.png',
   twitterCard: 'summary_large_image'
 })
 
